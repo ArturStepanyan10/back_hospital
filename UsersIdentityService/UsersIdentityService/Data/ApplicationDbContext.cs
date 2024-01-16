ec865488ac700d7d8358768de0642fc4ff48494f
Ваня Маркин <fakel0901@gmail.com>
2024-01-16 10:22:02 +0300

    Удалить лишнее

--- UsersIdentityService/UsersIdentityService/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace UsersIdentityService.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
